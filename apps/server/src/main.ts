a551592c6a24d7e7307377ef962d133879b958b2
VLaroye <laroye.vincent@gmail.com>
2022-08-03 10:17:20 +0200

    init 3 apps

---
+++ apps/server/src/main.ts
@@ -0,0 +1,44 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+import { dirname, join } from "path";
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to server!' });
+});
+
+const frontPath = dirname(require.resolve('../../front/project.json'))
+const front2Path = dirname(require.resolve('../../front2/project.json'))
+
+const frontBuiltFilesPath = `${frontPath}/build`
+const front2BuiltFilesPath = `${front2Path}/build`
+
+const serveFront = (app: express.Express) => {
+  app.use('/', express.static(frontBuiltFilesPath))
+
+  app.get('/*', function (req, res) {
+    res.sendFile(join(frontBuiltFilesPath, 'index.html'));
+  });
+}
+
+const serveFront2 = (app: express.Express) => {
+  app.use('/front2', express.static(front2BuiltFilesPath))
+
+  app.get('/front2*', function (req, res) {
+    res.sendFile(join(front2BuiltFilesPath, 'index.html'));
+  });
+}
+
+serveFront(app)
+serveFront2(app)
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
